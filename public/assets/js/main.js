f17416efbaa2497b44bff3a3eb29426f3dba248f
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-09-27 05:00:28 +0530

    fixing loader

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -353,12 +353,12 @@ Description: Gerold - Personal Portfolio HTML5 Template
     });
 
     // Step 2: Delay of 1.5 seconds
-    tl.to({}, { duration: 1 });  
+    tl.to({}, { duration: 1.4 });  
 
     // Step 3: Loading text sinks into the line and disappears
     tl.to(".loading-screen h3", {
         ease: "power4.inOut",
-        duration: 1.6,
+        duration: 1.8,
         y: "100%",  
         opacity: 0,  
     }, "<");  
